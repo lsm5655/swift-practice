a916bbae51f0373bb1aa3218333f1b05f7dfabc7
lsm5655 <lsm5655@naver.com>
2021-07-06 21:01:19 +0900

    07.06 swift

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -11,9 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Hello", message: "Second Alert", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
