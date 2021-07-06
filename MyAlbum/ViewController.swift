720954dbd3647e643f4c8cd72d7f77cd13cea01c
lsm5655 <lsm5655@naver.com>
2021-07-06 21:43:14 +0900

    closure

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -9,15 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 0
+    
+    @IBOutlet weak var price: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        refresh()
     }
 
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello", message: "Second Alert", preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let message = "가격은 ₩\(currentValue)입니다."
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {
+            action in self.refresh()
+        })
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func refresh() {
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        price.text = "₩ \(currentValue)"
     }
 }
 
